87139e05c869ff84308ce4a7a308a7c4ee5937f8
Jen_Tran <jentran.gamedeveloper@gmail.com>
2024-12-25 00:00:01 +0700

    ._.

--- Array.cpp
+++ Array.cpp
@@ -351,4 +351,52 @@ int main ()
         cout << endl; 
     }
     cout <<  "----------------------------------" << endl; 
+
+    int e;
+    cout << "Enter the number of elements: "; 
+    cin >> e; 
+
+    int max_length = e > 6 ? e : 6; 
+    int arr11[max_length] = {}; // Tạo ra một mảng rỗng
+    int arr12[max_length] = {}; // Tạo ra một mảng rỗng
+    int arr13[e] = {};
+    int arr14[2][max_length];
+
+    
+    for (int i = max_length - 6; i < max_length; i++)
+    {
+        arr11[i] = arr2[i - (max_length - 6)]; 
+    }
+   
+    cout << "Enter the value of elements of array10: "; 
+    for (int i = 0; i < e; i++)
+    {
+        cin >> arr13[i]; 
+    }
+    for (int i = max_length - e; i < max_length; i++)
+    {
+        arr12[i] = arr13[i - (max_length - e)]; 
+    }
+
+    for (int i = 0; i < max_length; i++)
+    {
+        arr14[0][i] = arr11[i];
+    }
+
+    for (int i = 0; i < max_length; i++)
+    {
+        arr14[1][i] = arr12[i]; 
+    }
+
+    cout << "arr14 =  "; 
+    cout << endl; 
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < max_length; j++)
+        {
+            cout << setw(4) << arr14[i][j] << " ";
+        }
+        cout << endl; 
+    }
+    cout <<  "----------------------------------" << endl;
 }
--- test.cpp
+++ test.cpp
@@ -10,37 +10,52 @@ int main()
 {
     int arr2[6] = {2, 3, 1, 4, 7, 9};
     int e;
-    cout << "Enter the number of elements of arr9: "; 
+    cout << "Enter the number of elements: "; 
     cin >> e; 
-    int arr9[e]; 
-    cout << "Enter the value of arr9: "; 
+
+    int max_length = e > 6 ? e : 6; 
+    int arr11[max_length] = {}; // Tạo ra một mảng rỗng
+    int arr12[max_length] = {}; // Tạo ra một mảng rỗng
+    int arr13[e] = {};
+    int arr14[2][max_length];
+
+    
+    for (int i = max_length - 6; i < max_length; i++)
+    {
+        arr11[i] = arr2[i - (max_length - 6)]; 
+    }
+   
+    cout << "Enter the value of elements of array10: "; 
     for (int i = 0; i < e; i++)
     {
-        cin >> arr9[i]; 
-    } 
-
-    int length_2 = sizeof(arr2) / sizeof(arr2[0]); 
-    int length_9 = sizeof(arr9) / sizeof(arr9[0]); 
-    int max_length = length_2 > length_9 ? length_2 : length_9; 
-    int arr11[2][max_length]; 
-    
-    for (int i = max_length - 1; i > max_length - length_2; i--)
+        cin >> arr13[i]; 
+    }
+    for (int i = max_length - e; i < max_length; i++)
     {
-        if (i > max_length - length_2 - 1)
-        { 
-            arr11[0][i] = arr2[max_length - length_2]
-        }
+        arr12[i] = arr13[i - (max_length - e)]; 
     }
 
-    cout << "arr11 = " << endl; 
+    for (int i = 0; i < max_length; i++)
+    {
+        arr14[0][i] = arr11[i];
+    }
+
+    for (int i = 0; i < max_length; i++)
+    {
+        arr14[1][i] = arr12[i]; 
+    }
+
+    cout << "arr14 =  "; 
+    cout << endl; 
     for (int i = 0; i < 2; i++)
     {
         for (int j = 0; j < max_length; j++)
         {
-            cout << setw(4) << arr10[i][j] << " "; 
+            cout << setw(4) << arr14[i][j] << " ";
         }
         cout << endl; 
     }
-    cout <<  "----------------------------------" << endl; 
 
 }
+
+
