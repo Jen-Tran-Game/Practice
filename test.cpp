871d475379e4354c9a871d2ab78d1cde553ce35d
Jen_Tran <jentran.gamedeveloper@gmail.com>
2024-12-25 21:55:05 +0700

    ._.

--- test.cpp
+++ test.cpp
@@ -8,45 +8,8 @@ using namespace std;
 
 int main()
 {
-    int arr15[11];
-    cout << "Enter the value of arr15: ";
-    for (int i = 0; i < 11; i++)
-    {
-        cin >> arr15[i];
-    }
+    
 
-    cout << "arr15 = ";
-    for (int i = 0; i < 11; i++)
-    {
-        cout << arr15[i]; 
-    }
-    cout << endl; 
-
-    if (arr15[0] == 0)
-    {
-        string lastLine = "+1"; 
-        for (int i = 2; i < 11; i++)
-        {
-            lastLine += to_string(arr15[i]);
-        }
-        cout << lastLine; 
-        ofstream write_sdt("sdt.txt");
-        write_sdt << lastLine; 
-        write_sdt.close();
-    }
-    else
-    {
-        string lastLine = "+84";
-        for (int i = 2; i < 11; i++)
-        {
-            lastLine += to_string(arr15[i]);
-        }
-        cout << lastLine;
-        ofstream write_sdt("sdt.txt"); 
-        write_sdt << lastLine; 
-        write_sdt.close();
-    }
-    cout << endl << "----------------------------------" << endl;
 }
 
 
