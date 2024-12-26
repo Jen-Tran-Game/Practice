24755548d5eef2a7cf89c9c3331d277b7df49213
Jen_Tran <jentran.gamedeveloper@gmail.com>
2024-12-26 22:02:08 +0700

    ._.

--- Array.cpp
+++ Array.cpp
@@ -6,6 +6,13 @@
 
 using namespace std; 
 
+void enterArray (int arr[], int n, string name_in);
+void printString (int arr[], int n, string &lastLine);
+void testFirst (int arr[], int n, string &lastLine);
+void writeFile_func (string lastLine, string name_file);
+void enterArray (int arr[], int n, string name_in);
+void printArray (int arr[], int n, string name_out);
+
 void enterArray (int arr[], int n, string name_in)
 {
     cout << "Enter the value of " << name_in << ": "; 
@@ -24,6 +31,49 @@ void printArray (int arr[], int n, string name_out)
         cout << arr[i] << " ";
     }
     cout << endl << "----------------------------------" << endl
+}
+
+void enterArray (int arr[], int n, string name_in)
+{
+    cout << "Enter the value of " << name_in << ": "; 
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i]; 
+    }
+}
+
+void writeFile_func (string lastLine, string name_file)
+{
+    cout << lastLine; 
+    ofstream write_point(name_file);
+    write_point << lastLine; 
+    write_point.close(); 
+}
+
+void printString (int arr[], int n, string &lastLine)
+{
+    for (int i = 2; i < n; i++)
+    {
+        lastLine += to_string(arr[i]);
+    }
+}
+
+void testFirst (int arr[], int n, string &lastLine, string name_file)
+{
+    switch (arr[0])
+    {
+        case 0: 
+            lastLine = "+1";
+            printString (arr, n, lastLine);
+            writeFile_func (lastLine, name_file);
+            break;
+        case 1: 
+            lastLine = "+84";
+            printString (arr, n, lastLine);
+            writeFile_func (lastLine, name_file);
+            break;
+    }
 }
 
 int main ()
@@ -381,6 +431,7 @@ int main ()
     cout <<  "----------------------------------" << endl;
 
     int arr15[11];
+
     // Nhập giá trị của mảng arr15
     enterArray (arr15, 11, "arr15");
     cout << "arr15 = ";
@@ -390,31 +441,9 @@ int main ()
     }
     cout << endl; 
 
-    if (arr15[0] == 0)
-    {
-        string lastLine = "+1"; 
-        for (int i = 2; i < 11; i++)
-        {
-            lastLine += to_string(arr15[i]);
-        }
-        cout << lastLine; 
-        ofstream write_sdt("sdt.txt");
-        write_sdt << lastLine; 
-        write_sdt.close();
-    }
-    else
-    {
-        string lastLine = "+84";
-        for (int i = 2; i < 11; i++)
-        {
-            lastLine += to_string(arr15[i]);
-        }
-        cout << lastLine;
-        ofstream write_sdt("sdt.txt"); 
-        write_sdt << lastLine; 
-        write_sdt.close();
-    }
-
-    cout <<  "----------------------------------" << endl;
+    string lastLine;
+    string write_sdt;
+    testFirst (arr15, 11, lastLine, "sdt.txt");
+    cout << endl <<"----------------------------------" << endl;
 
 }
