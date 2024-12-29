5a6ae3e6e9349a83610d6c1e8267b17a011a4e84
Jen_Tran <jentran.gamedeveloper@gmail.com>
2024-12-29 22:47:25 +0700

    ._.

--- Password.cpp
+++ Password.cpp
@@ -1,13 +1,41 @@
 #include <iostream> 
 #include <string> 
 #include <fstream> 
- 
+#include <vector> 
+
 using namespace std; 
 
 int main ()
-{
-    int n; 
-    int password[n];
-
+{ 
+    // Nhập password
+    string password; 
+    cout << "Enter your password: ";
+    cin >> password;
     
+    // Ghi password vào file txt
+    fstream write_pw("password.txt", fstream::out);
+    write_pw << password; 
+    write_pw.close(); 
+
+    // Nhập lại password
+    string inputPassword; 
+    cout << "Your password is: "; 
+    cin >> inputPassword; 
+
+    // Đọc password trong file txt
+    string storedPassword;
+    fstream read_pw("password.txt", fstream::in);
+    getline (read_pw, storedPassword);
+    read_pw.close();
+
+    // Kiểm tra xem password nhập vào có giống với password được tạo không 
+    if (inputPassword == storedPassword)
+    {
+        cout << "Login successfully";
+    }
+    else
+    {
+        cout << "Unauthorized";
+        
+    }    
 }
