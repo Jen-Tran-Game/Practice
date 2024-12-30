0ea34318a12985d572a32156185b1ea8056e6927
Jen_Tran <jentran.gamedeveloper@gmail.com>
2024-12-30 18:49:44 +0700

    ._.

--- Password.cpp
+++ Password.cpp
@@ -5,37 +5,71 @@
 
 using namespace std; 
 
-int main ()
-{ 
-    // Nhập password
-    string password; 
-    cout << "Enter your password: ";
+void enterPassword_signup (string password);
+void write_pw_func (string password);
+void enterPassword_login (string &inputPassword); 
+void read_pw_func (string storedPassword);
+
+void enterPassword_signup (string password)
+{
+    cout << "Enter your new password: ";
     cin >> password;
-    
-    // Ghi password vào file txt
+    write_pw_func (password);
+}
+
+void write_pw_func (string password)
+{
     fstream write_pw("password.txt", fstream::out);
     write_pw << password; 
-    write_pw.close(); 
+    write_pw.close();
+}
 
-    // Nhập lại password
-    string inputPassword; 
-    cout << "Your password is: "; 
+void enterPassword_login (string &inputPassword)
+{
+    cout << "Enter your password you set: ";
     cin >> inputPassword; 
+}
 
-    // Đọc password trong file txt
-    string storedPassword;
-    fstream read_pw("password.txt", fstream::in);
-    getline (read_pw, storedPassword);
-    read_pw.close();
-
-    // Kiểm tra xem password nhập vào có giống với password được tạo không 
-    if (inputPassword == storedPassword)
+int main ()
+{     
+    // Lựa chọn đăng nhập hoặc đăng ký 
+    int mode; 
+    string password; 
+    string inputPassword; 
+    cout << "Press 0: sign up" << endl; 
+    cout << "Press 1: log in" << endl; 
+    cout << "Your option: "; 
+    cin >> mode; 
+    if (mode == 0)
     {
-        cout << "Login successfully";
+        enterPassword_signup (password);
+        cout << "Set password successfully";
     }
     else
     {
-        cout << "Unauthorized";
+        enterPassword_login (inputPassword); 
+
+        // Đọc password trong file txt
+        string storedPassword;
+        fstream read_pw("password.txt", fstream::in);
+        getline (read_pw, storedPassword);
+        read_pw.close();
+
+        // Kiểm tra xem password nhập vào có giống với password được tạo không
+        if (inputPassword == storedPassword)
+        {
+            cout << "Login successfully";
+        }
+        else
+        {
+            cout << "Unauthorized";
         
-    }    
+        }   
+    }
+
+    
+    
+
+     
+     
 }
