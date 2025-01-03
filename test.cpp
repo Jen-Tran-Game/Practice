64080f4e89a7d653cfb72f28e61ab929bc1c8d5c
Jen_Tran <jentran.gamedeveloper@gmail.com>
2025-01-03 22:40:31 +0700

    ._.

--- test.cpp
+++ test.cpp
@@ -7,16 +7,19 @@ using namespace std;
 
 int main ()
 {
-    string username, password, inputUsername, inputPassword, storedPassword; 
+    string username, password, inputUsername, inputPassword, storedPassword;
+    vector <string> usernames; 
+    vector <string> passwords;  
     int mode; 
     cout << "Press 0: Sign up" << endl << "Press 1: Log in " << endl; 
     cin >> mode; 
-    fstream write_usr_pw ("Users.txt", fstream::out); 
-    write_usr_pw.close();
     if (mode == 0)
     {
         cout << "Create username: "; 
-        cin >> username; 
+        cin >> username;
+        fstream write_usr_pw ("Users.txt", fstream::out );
+        write_usr_pw << "Username: " << username; 
+        write_usr_pw.close(); 
 
     }
 
