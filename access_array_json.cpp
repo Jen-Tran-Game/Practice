aa5ce8cd20ed693ea95aa7c0e6ea7ed467ff9ef1
Jen_Tran <jentran.gamedeveloper@gmail.com>
2025-01-19 18:26:24 +0700

    ._.

---
+++ access_array_json.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "json.hpp"
+
+using namespace std; 
+using json = nlohmann::json; 
+
+int main ()
+{
+    json j = json::array({"C++", "Python", "JavaScript"});
+
+    string firstLang = j[0]; 
+    string secondLang = j[1]; 
+    string thirdLang = j[2];
+
+    cout << "First Language: " << firstLang << endl; 
+    cout << "Second Language: " << secondLang << endl; 
+    cout << "Third Language: " << thirdLang << endl; 
+    
+    return 0; 
+}
---
+++ access_object_json.cpp
@@ -0,0 +1,18 @@
+#include <iostream> 
+#include "json.hpp"
+
+using namespace std; 
+using json = nlohmann::json; 
+
+int main ()
+{
+    json j; 
+    j["name"] = "Jen"; 
+    j["age"] = 21; 
+    j["school"] = "HCMUTE";
+
+    string name = 
+
+    cout << "Name: " << name
+    return 0; 
+}
