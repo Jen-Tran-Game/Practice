fc6300159b2db7248fd73ae31748d1c73a96cadb
Jen_Tran <jentran.gamedeveloper@gmail.com>
2025-01-22 22:11:42 +0700

    SHA256_1

--- test_json.cpp
+++ test_json.cpp
@@ -1,10 +1,31 @@
 #include <iostream> 
 #include "json.hpp"
 #include <fstream> 
+#include <openssl/hmac.h>
+#include <openssl/evp.h>
+#include <iomanip>
 
 using namespace std; 
 using ordered_json = nlohmann::ordered_json; // Để giữ thứ tự trong lúc đọc và ghi file 
 
+std::string encode_password(const std::string &password, const std::string &key)
+{
+    unsigned char result[EVP_MAX_MD_SIZE];
+    unsigned int result_len = 0; 
+
+    //Tạo đối tượng HMAC với key và thuật toán SHA256
+    HMAC_CTX *ctx = HMAC_CTX_new(); 
+    HMAC_Init_ex(ctx, key.data(), key.size(), EVP_sha256(), nullptr);
+    HMAC_Update(ctx, result, &result_len);
+    HMAC_CTX_free(ctx);
+
+    //Chuyển đổi giá trị mã hoá sang chuỗi hexadecimal
+    std::ostringstream hex_stream; 
+    for (unsigned int i = 0; i < result_len; ++i)
+    {
+        hex_stream << std::hex << std::setw(2) << std::setfill('0') << 
+    }
+}
 int main()
 {   
     // Read and Print username and password you want 
