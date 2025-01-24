9741ec911e1c7224cbe6119e27a74829c2a77b9e
Jen_Tran <jentran.gamedeveloper@gmail.com>
2025-01-24 22:19:55 +0700

    ._.

--- test_json.cpp
+++ test_json.cpp
@@ -3,7 +3,7 @@
 #include <iomanip>
 #include <sstream>
 #include "json.hpp"
-#include "sha.h"
+#include <openssl/sha.h>
 
 using namespace std; 
 using ordered_json = nlohmann::ordered_json; // Để giữ thứ tự trong lúc đọc và ghi file 
---
+++ test_struct.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std; 
+
+struct Person
+{
+    string name;
+    int age; 
+    float height; 
+};
+
+int main ()
+{
+    Person person; 
+    
+
+    return 0; 
+}
